79cca68092094862fa770dfddaca695ac14fa995
Boitumelo <boityy.js@gmail.com>
2024-01-08 16:37:41 +0200

    changed css and added additional javascript

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 function displayPoem(response) {
-new Typewriter("#poem", {
-  strings: response.data.answer,
-  autoStart: true,
-  delay: 1,
-  cursor: "",
-});
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 function generatePoem(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    let instructionsInput = document.querySelector("#user-instructions");
-    let apiKey = "3a5627ae326058co2dc72a44fbta8eef";
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "3a5627ae326058co2dc72a44fbta8eef";
   let context =
-    "You are an English Elizabethan era poem expert that writes short poems";
+    "You are an English Elizabethan era poem expert that writes short and appropriate poems";
   let prompt = `User instructions: Please generate a short poem about ${instructionsInput}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(displayPoem)
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating"> Generating an Elizabethan poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
-poemFormElement.addEventListener("submit", generatePoem)
+poemFormElement.addEventListener("submit", generatePoem);
