670f06f2c7874a1bcfc6854b07f65686f60de608
Boitumelo <boityy.js@gmail.com>
2024-01-08 16:09:39 +0200

    added API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-function generatePoem(event) {
-    event.preventDefault();
-
-
+function displayPoem(response) {
 new Typewriter("#poem", {
-  strings: "Your poem is here",
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: "",
 });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "3a5627ae326058co2dc72a44fbta8eef";
+  let context =
+    "You are an English Elizabethan era poem expert that writes short poems";
+  let prompt = `User instructions: Please generate a short poem about ${instructionsInput}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem)
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
