7f6cec43c294c30ef32fb1155899d8b348c2acc2
Boitumelo <boityy.js@gmail.com>
2024-01-08 14:17:32 +0200

    added javascript to submit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function generatePoem(event) {
+    event.preventDefault();
+
+
+new Typewriter("#poem", {
+  strings: "Your poem is here",
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem)
